3ab9d857e22a8b337b9bdf65d5f4701e6fdde903
Amruta Khande <amrutakhande3@gmail.com>
2025-07-24 23:03:24 +0530

    Assignment Practice

---
+++ Javascript/Part9/app.js
@@ -0,0 +1,10 @@
+let links = document.querySelectorAll('.box a');
+
+// for(let i=0; i<links.length; i++) {
+//     links[i].style.color = "purple";
+// }
+
+
+for (link of links) {
+    link.style.color = "green";
+}
