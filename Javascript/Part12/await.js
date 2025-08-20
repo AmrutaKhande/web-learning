e7e82a4dacab8f48971cf09318ad504cf173dae2
Amruta Khande <amrutakhande3@gmail.com>
2025-08-20 18:35:27 +0530

    Api Request

--- Javascript/Part12/await.js
+++ Javascript/Part12/await.js
@@ -1,19 +1,19 @@
 function getNum() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const num = Math.floor(Math.random() * 10) + 1;
-            console.log(num);
-            resolve();
-        }, 1000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const num = Math.floor(Math.random() * 10) + 1;
+      console.log(num);
+      resolve();
+    }, 1000);
+  });
 }
 
 async function demo() {
-    await getNum();
-    await getNum();
-    await getNum();
-    await getNum();
-    await getNum();
+  await getNum();
+  await getNum();
+  await getNum();
+  await getNum();
+  await getNum();
 }
 
 // color change function using async/await
@@ -21,19 +21,31 @@ async function demo() {
 h1 = document.querySelector("h1");
 
 async function changeColor(color, delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            h1.style.color = color;
-            console.log(`color changed to ${color}!`);
-            resolve("color changed!");
-        }, delay);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 10) + 1;
+      if (num > 5) {
+        reject("promise rejected");
+      }
+      h1.style.color = color;
+      console.log(`color changed to ${color}!`);
+      resolve("color changed!");
+    }, delay);
+  });
 }
 
-async function demo() { 
+async function demo() {
+  try {
     await changeColor("red", 1000);
     await changeColor("orange", 1000);
     await changeColor("green", 1000);
     await changeColor("blue", 1000);
     await changeColor("purple", 1000);
+  } catch (err) {
+    console.log("error caught");
+    console.log(err);
+  }
+  let a = 5;
+  console.log(a);
+  console.log("new number = ", a + 3);
 }
